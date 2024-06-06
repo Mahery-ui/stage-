7b25cf7dade9d93947dbddd51a06074647209ec9
Mahery-ui <rasolofoniainamahery118@gmail.com>
2024-06-06 15:49:21 +0200

    Updating stage

--- polls/views.py
+++ polls/views.py
@@ -1,50 +1,28 @@
-from django.shortcuts import render
-from django.http import Http404
 # Create your views here.
-from django.http import HttpResponse
-from .models import Question, Choice
-from django.template import loader 
+from .models import Question, Choice 
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 
+    
+class IndexView(generic.ListView):
+    template_name= 'polls/index.html'
+    contex_object_name= 'latest_question_list'
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+    
+class DetailView(generic.DetailView):
+    model= Question
+    template_name= 'polls/detail_html'
 
-def detail(request,question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
-
-def results(request,request_id):
-    response="You're looking at the resultsof question %s."
-    return HttpResponse(response % question_id)
+class ResultsView(generic.DateDetailView):
+    model= Question
+    template_name= 'polls/results.html'
 
 def vote(request,request_id):
-    return HttpResponse("You're voting on question %s." % question_id) 
-
-def index(request):
-    latest_question_list=Question.objects.order_by('-pub_date')[:5]
-    output= ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
-
-def index(request):
-    latest_question_list= Question.objects.order_by('-pub_date')[:5]
-    template= loader.get_template('polls/index.html')
-    context= {'latest_question_list': latest_question_list,}
-    return HttpResponse(template.render(context, request))
-
-def index(request):
-    latest_question_list= Question.objects.order_by('-pub_date')[:5]
-    context= {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
-
-def detail(request, question_id):
-    try:
-        question= Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request,'polls/detail.html', {'question':question})
-
-def detail(request, question_id):
-    question=get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question' : question})
+    return HttpResponseRedirect("You're voting on question %s." % question_id) 
 
 def vote(request, question_id):
     question=get_object_or_404(Question, pk=question_id)
