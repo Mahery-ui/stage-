61d855fbc14cc496b20e92675cca66ec3bc906cf
Mahery-ui <rasolofoniainamahery118@gmail.com>
2024-06-10 11:35:16 +0200

    ading tests

--- polls/views.py
+++ polls/views.py
@@ -1,19 +1,32 @@
-# Create your views here.
-from .models import Question, Choice
-from django.template import loader 
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 
-def detail(request,question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+from .models import Choice, Question
 
-def results(request,request_id):
-    response="You're looking at the resultsof question %s."
-    return HttpResponse(response % question_id)
 
-def vote(request,request_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
+def vote(request, question_id):
+    ... # same as above, no changes needed.
 
 def vote(request, question_id):
     question= get_object_or_404(Question, pk=question_id)
@@ -29,18 +42,3 @@ def vote(request, question_id):
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args= (question.id,)))
 
-
-def index(request):
-    latest_question_list=Question.objects.order_by('-pub_date')[:5]
-    output= ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
-
-def index(request):
-    latest_question_list= Question.objects.order_by('-pub_date')[:5]
-    template= loader.get_template('polls/index.html')
-    context= {'latest_question_list': latest_question_list,}
-    return HttpResponse(template.render(context, request))
-
-def results(request, question_id):
-    question= get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
