bb593cf9d4a72e4d8bb6bbee0c67c335e5ed0d66
Mahery-ui <rasolofoniainamahery118@gmail.com>
2024-06-10 13:52:25 +0200

    Modifing models

--- polls/models.py
+++ polls/models.py
@@ -8,7 +8,7 @@ class Question(models.Model):
     pub_date=models.DateTimeField('date published')
     def was_published_recently(self):
         now= timezone.now
-        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+        return now-datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Choice(models.Model):
     question=models.ForeignKey(Question, on_delete=models.CASCADE)
