70709714357e929703f01d0b38967c0f6f990c30
Mahery-ui <rasolofoniainamahery118@gmail.com>
2024-06-12 15:12:40 +0200

    update

--- polls/admin.py
+++ polls/admin.py
@@ -5,3 +5,14 @@ from .models import Choice
 admin.site.register(Question)
 admin.site.register(Choice)
 # Register your models here.
+
+class QuestionAdmin(admin, ModelAdmin):
+    fields= ['pub_date', 'question_text']
+admin.site.register(Question, QuestionAdmin)
+
+class QuestionAdmin(admin, ModelAdmin):
+    fieldsest= [
+        (None,                {'fields': ['question_text']}), 
+        ('Date information',  {'fields': ['pub_date']}), 
+    ]
+admin.site.register(Question, QuestionAdmin)
